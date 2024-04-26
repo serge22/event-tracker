9cc5f7e0cd771ee818cf8643473d508a1a1c4d2a
serge <7113454@gmail.com>
2024-04-26 11:26:18 +0600

    events soft delete

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,10 +6,12 @@ use App\Models\Scopes\OwnerScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'user_id',
